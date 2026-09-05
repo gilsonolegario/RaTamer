65de6f69c62bbd4d12031a87d3927bdd444ddda1
gilsonolegario <gilsonolegario@gmail.com>
2026-09-05 08:53:27 -0300

    chore(release): v1.0.4 (7)

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -56,8 +56,8 @@ cat > "$APP/Contents/Info.plist" <<'EOF'
     <key>CFBundleExecutable</key><string>RaTamer</string>
     <key>CFBundleIconFile</key><string>RaTamer</string>
     <key>CFBundlePackageType</key><string>APPL</string>
-    <key>CFBundleShortVersionString</key><string>1.0.3</string>
-    <key>CFBundleVersion</key><string>6</string>
+    <key>CFBundleShortVersionString</key><string>1.0.4</string>
+    <key>CFBundleVersion</key><string>7</string>
     <key>LSMinimumSystemVersion</key><string>14.0</string>
 </dict>
 </plist>
